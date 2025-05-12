a691e824d19e34a17c3b3ddfb386cf79e0b08179
PedroFDias <phf.dias.2022@gmail.com>
2025-05-12 17:00:47 -0300

    update header: ficar header e scroll dinamico

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,20 +1,36 @@
-import './cabecalho.css';
+import { useEffect, useState } from 'react';
+import styles from './Cabecalho.module.css';
 
 export function Header() {
+
+    const [scrolled, setScroll] = useState(false);
+
+    useEffect(() => {
+        const Scrolled = () => {
+            setScroll(window.scrollY > 0);
+        }
+
+        window.addEventListener("scroll", Scrolled);
+
+        return () => {
+            window.removeEventListener("scroll", Scrolled); // cleanup
+        };
+    })
+
     return (
-        <header id="cabecalho">
-        <div className="container">
-            <div className="header">
-                <a id="nome" href="index.html"><strong>Pedro Dias</strong></a>
-                <button className="hamburguer"></button>
-                <nav id="menu">
-                    <a href="">Inicio</a>
-                    <a href="#MeusProjetos">Projetos</a>
-                    <a href="#Contato">Contato</a>
-                    <p className="direitos">© 2025 Pedro Dias. Todos os direitos reservados.</p>
-                </nav>
+        <header className={`${styles.cabecalho} ${scrolled ? styles.cabecalhoScroll : ''}`}>
+            <div className="container">
+                <div className={styles.header}>
+                    <p className={styles.nome}>Pedro Dias</p>
+                    <button className={styles.hamburguer}></button>
+                    <nav className={styles.menu}>
+                        <a href="">Inicio</a>
+                        <a href="#MeusProjetos">Projetos</a>
+                        <a href="#Contato">Contato</a>
+                        <p className="direitos">© 2025 Pedro Dias. Todos os direitos reservados.</p>
+                    </nav>
+                </div>
             </div>
-        </div>
-    </header>
+        </header>
     );
 }
