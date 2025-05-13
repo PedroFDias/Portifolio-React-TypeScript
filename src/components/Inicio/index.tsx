2327f21286d51e07c514437edb9e9c1a4c7961be
PedroFDias <phf.dias.2022@gmail.com>
2025-05-13 13:42:02 -0300

    update inicio: Adicionar lib typed para "eu sou"

--- src/components/Inicio/index.tsx
+++ src/components/Inicio/index.tsx
@@ -1,7 +1,8 @@
+import { ReactTyped } from "react-typed"
 import { BotaoLink } from '../BotaoLink';
 import './Inicio.css';
 
-export function Inicio () {
+export function Inicio() {
     return (
         <div id="inicio" className="container">
             <div className="container" id="apresentacao">
@@ -9,20 +10,27 @@ export function Inicio () {
                     <div className="apresentacao-script">
                         <h6>Olá, eu me chamo</h6>
                         <h1>Pedro Dias</h1>
-                        <h3>E sou <span className="text"></span> </h3>
-                    </div>
-                    <div className="resumo">
-                        <p>Atualmente cursando Análise e Desenvolvimento de Sistemas na PUC-Minas, com foco em
-                            desenvolvimento backend e experiências em projetos práticos.
-                        </p>
+                        <h3>E sou <span className="text">
+                            <ReactTyped
+                                strings={['Desenvolvedor de Software', 'Estudante de Programação']}
+                                typeSpeed={50}
+                                backSpeed={50}
+                                loop
+                            />
+                        </span> </h3>
+                        <div className="resumo">
+                            <p>Atualmente cursando Análise e Desenvolvimento de Sistemas na PUC-Minas, com foco em
+                                desenvolvimento backend e experiências em projetos práticos.
+                            </p>
+                        </div>
                     </div>
                     <div id="linksIcons">
                         <BotaoLink link="https://github.com/PedroFDias" icon="bi bi-github" />
-                        <BotaoLink link="http://www.linkedin.com/in/pedrofdias" icon="bi bi-linkedin"/>
+                        <BotaoLink link="http://www.linkedin.com/in/pedrofdias" icon="bi bi-linkedin" />
                     </div>
                     <div className="circulo-fora">
                         <div id="imagemPerfil" className="circulo-dentro">
-                            <img id="imgPerfil" src="../../public/imagens/perfil2.png" alt="Foto de Perfil" className="w-full h-full rounded-full object-cover"/>
+                            <img id="imgPerfil" src="../../public/imagens/perfil2.png" alt="Foto de Perfil" className="w-full h-full rounded-full object-cover" />
                         </div>
                     </div>
                     <a className="maisSobre" href="">Mais Sobre mim</a>
