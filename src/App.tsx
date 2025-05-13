541a1aa789795192e53d74ec77fd7ab4a1eec6d3
PedroFDias <phf.dias.2022@gmail.com>
2025-05-13 13:42:39 -0300

    update App: adicionar contato e footer ao projeto

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Header } from './components/Cabecalho';
 import { Inicio } from './components/Inicio';
 import { SobreMim } from './components/SobreMim';
 import { Projeto } from './components/Projetos';
+import Contato from './components/Contato'
+import Footer from './components/Footer';
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <Inicio />
       <SobreMim/>
       <Projeto/>
+      <Contato/>
+      <Footer/>
     </>
   )
 }
