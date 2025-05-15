8b2077c44d676863c2337ca93f74b4cad4ac91ad
PedroFDias <phf.dias.2022@gmail.com>
2025-05-15 01:29:15 -0300

    update improve mergulhadora: adequar para display none

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -18,12 +18,28 @@ export default function Contato() {
                     whileInView={{ opacity: 1, y: 0, transition: { delay: 0.2, duration: 1.5 } }}
                     viewport={{ once: false, amount: .2 }}
                     className="Contatos">
-                    <a href="https://wa.me/31989090902"><i className="bi bi-whatsapp"> (31) 98909-0902</i></a>
-                    <a href="mailto:phf.dias.2022@gmail.com"><i
-                        className="bi bi-envelope-at">phf.dias.2022@gmail.com</i></a>
-                    <a href="http://www.linkedin.com/in/pedrofdias"><i className="bi bi-linkedin"> pedrofdias</i></a>
+                    <motion.a
+                        initial={{ opacity: 0, y: 30 }}
+                        whileInView={{ opacity: 1, y: 0, transition: { delay: 0.2, duration: 1.5 } }}
+                        viewport={{ once: false, amount: .2 }}
+                        href="https://wa.me/31989090902"><i className="bi bi-whatsapp"> (31) 98909-0902</i></motion.a>
+                    <motion.a
+                        initial={{ opacity: 0, y: 30 }}
+                        whileInView={{ opacity: 1, y: 0, transition: { delay: 0.2, duration: 1.5 } }}
+                        viewport={{ once: false, amount: .2 }}
+                        href="mailto:phf.dias.2022@gmail.com"><i
+                            className="bi bi-envelope-at">phf.dias.2022@gmail.com</i></motion.a>
+                    <motion.a
+                        initial={{ opacity: 0, y: 30 }}
+                        whileInView={{ opacity: 1, y: 0, transition: { delay: 0.2, duration: 1.5 } }}
+                        viewport={{ once: false, amount: .2 }}
+                        href="http://www.linkedin.com/in/pedrofdias"><i className="bi bi-linkedin"> pedrofdias</i></motion.a>
 
-                    <a><i className="bi bi-instagram"> pedrofdias</i></a>
+                    <motion.a
+                        initial={{ opacity: 0, y: 30 }}
+                        whileInView={{ opacity: 1, y: 0, transition: { delay: 0.2, duration: 1.5 } }}
+                        viewport={{ once: false, amount: .2 }}
+                    ><i className="bi bi-instagram"> pedrofdias</i></motion.a>
                 </motion.div>
                 <img className='mergulhadora' src="../../public/imagens/mergulhadora2.png" alt="" />
             </div>
