d8c6d638f4b57a7754ffca7b4fe00ddeff51d890
PedroFDias <phf.dias.2022@gmail.com>
2025-05-15 22:02:14 -0300

    update sobre Mim: adicionar tecnologias aprendidas recentemente

--- src/components/SobreMim/index.tsx
+++ src/components/SobreMim/index.tsx
@@ -12,16 +12,13 @@ export function SobreMim() {
                     viewport={{ once: false, amount: .4 }}
                     className="info">
                     <h2>Sobre mim</h2>
-                    <p>Sou um desenvolvedor em formação, apaixonado por backend e pela criação de soluções que conectam
-                        dados a pessoas. Tenho experiência acadêmica com APIs RESTful, integração de bancos relacionais,
-                        consumo de APIs externas e sistemas CRUD. Trabalho com Java, Spring Boot, JavaScript, HTML/CSS,
-                        PostgreSQL e MySQL. Também tenho familiaridade com metodologias ágeis como Scrum.</p>
+                    <p>Sou um desenvolvedor em formação, apaixonado por backend e pela criação de soluções que conectam dados a pessoas. Trabalho com <span className="tec1">&lt;Java + Spring&gt;</span>, <span className="tec2">&lt;JavaScript + Express&gt;</span>, <span className="tec3">&lt;TypeScript + React&gt;</span>, PostgreSQL, MySQL e MongoDB, além de ter familiaridade com metodologias ágeis como o Scrum. Tenho experiência acadêmica e pessoal com API's REST, integração com bancos, consumo de APIs externas e sistemas CRUD.</p>
                     <img src="imagens/desenvolvedor.png" alt="" />
                 </motion.div>
                 <motion.div
                     initial={{ opacity: 0, y: 15 }}
                     whileInView={{ opacity: 1, y: 0, transition: { duration: 1.5 } }}
-                    viewport={{ once: false, amount: .4}}
+                    viewport={{ once: false, amount: .4 }}
                     className="tecno">
                     <h2>Tecnologias</h2>
                     <div className="carrossel">
